f091df3ffdeca23474ef7b2f513dab42182b8c13
kaluarichama <kaluarichama@gmail.com>
2022-02-25 00:24:43 +0300

    interview prep coverage hilt,notifications,paging,mvvm

---
+++ app/src/main/java/com/luka/berry/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.luka.berry.di
+
+import com.luka.berry.network.UnsplashApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(UnsplashApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideUnSplashApi(retrofit: Retrofit): UnsplashApi =
+        retrofit.create(UnsplashApi::class.java)
+
+}
